7bab706afa4980b566dbff439235fca1a409c332
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-10-23 15:49:58 +0200

    Started ws 6

---
+++ MemoryLayout/Worksheet06/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+ 
+void recurse(int level) { 
+    char arry[10240]; 
+    printf("%d\n", level); 
+    recurse(level + 1); 
+} 
+ 
+int main(void) { 
+    recurse(1); 
+    return 0; 
+} 
