786a23760765d211f98fdbcb56debb0bbbb96ba3
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-11-10 14:32:09 +0100

    lect 10.11.25

---
+++ Testing/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void f2(int age)
+{
+	int test = age-age;
+	age = age / test;
+}
+void f1(int age)
+{
+	int local = 10;
+	f2(age+local);
+}
+int main(int argc, char **argv)
+{
+	int age = 33;
+	f1(age);
+	return 0;
+}
